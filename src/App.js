60871e3e6d4582e8125a9c81113d7d32fc562e17
amir <officer.k99@gmail.com>
2022-04-30 08:43:59 -0700

    project finished

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Counter from './containers/Counter/Counter'
+
+import './App.css'
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Counter />
+      </div>
+    )
+  }
+}
+
+export default App
